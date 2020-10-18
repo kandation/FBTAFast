5c63087596cfe28e7cce604c5b7c7d4387b74d4b
kandation <akkhaporn@gmail.com>
2020-10-18 18:42:43 +0700

    fix: :art: convert project for vscode

---
+++ Labs/Story/630426_chrome_story/630426_chrome_but_no_script.py
@@ -0,0 +1,91 @@
+from parsel import Selector
+from pymongo import MongoClient
+from urllib.parse import unquote
+# import bson
+from bson import DBRef, ObjectId
+
+from fbta_configs import FBTAConfigs
+from fbta_driver import FBTADriver
+from fbta_node_master import FBTANodeMaster
+from fbta_settings import FBTASettings
+
+fb_url_m = 'https://m.facebook.com/'
+fb_url_w = 'https://www.facebook.com/'
+
+db_name = 'fbta_20200331_2306'
+
+if __name__ == '__main__':
+    images = []
+
+    fb_url_w = 'https://www.facebook.com'
+
+    settings = FBTASettings('fadehara')
+    configs = FBTAConfigs()
+
+    settings.kill_driver_on_end = True
+    settings.driver_path = r'./Driver/chromedriver_76.exe'
+    settings.dir_cookies = r'./cookies/'
+    settings.use_nodeMaster_loadCookie = True
+
+    settings.use_nodeMaster = False
+    settings.init_node_master_browser = False
+
+    settings.renew_index = False
+    settings.fast_worker = True
+    # settings.date_process = [2017, 10, 1]
+    settings.dir_path_detail = settings.DIR_DETAIL_NEW_ALL_RUN
+
+    node_master = FBTANodeMaster(settings, configs)
+    node_master.start()
+
+    session = FBTADriver(node_master)
+
+    session.add_cookie_from_file()
+    session.get('https://m.facebook.com')
+    session.delete_cookie('noscript')
+    session.set_header_chrome()
+    # session.set_header_firefox()
+
+    #
+    # URL FOR LAB
+    #
+    import pathlib
+
+    # Test Story
+    url = 'https://m.facebook.com/story.php?story_fbid=3461671277199349&substory_index=43&id=100000695314425'
+    session.get(url)
+
+    dir = pathlib.Path(__file__).parent.absolute()
+    print(dir)
+    # exit()
+    with open(f'{dir}/story_chome.html', mode='w') as fo:
+        fo.write(session.page_source)
+    exit()
+
+    # headers = {
+    #     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
+    # }
+
+    page_error_topic = ['Content Not Found']
+
+    client = MongoClient()
+    db = client.get_database(db_name)
+    # collection = db.get_collection('03_post_page')
+    # coll_album = db.get_collection('99_album_no_duplicate')
+    #
+    # docs_post = coll_album.find()
+    #
+    # PHOTO_PATTERN = '\/([0-9]+)\/|\?fbid=([0-9]+)'
+    #
+    # url_patt = 'media/set/?set={a_type}.{aid}&type=1'
+    # allow_type = ['a', 'oa', 'pcb']
+    #
+    # for doc in docs_post:
+    #     a_type = doc.get('type')
+    #     aid = doc.get('aid')
+    #
+    #     url = fb_url_m + url_patt.format(a_type=a_type, aid=aid)
+    #
+    #     data_insert = {'photo-cluster': [find12img(url)]}
+    #
+    #     coll_album.update_one({'_id': doc.get('_id')}, {'$set': data_insert})
---
+++ fbta/fbta_110_03_user_info_album.py
@@ -0,0 +1,81 @@
+"""
+
+"""
+import unittest
+from datetime import datetime
+from time import time
+import re as regex
+from urllib.parse import parse_qs
+
+from pymongo import MongoClient
+
+from fbta_log import log
+
+
+class FBTA11001UserInfoAlbum:
+    def __init__(self):
+        """ Find user-info from url in URL_PHOTO_TANK """
+        pass
+
+    def get_user_info(self, url):
+        urls = str(url).split('/')
+
+        user_info = {'user-info': {}}
+        PATT_GROUPS = '\/groups\/(.+)\?'
+
+        if 'groups/' in url:
+
+            key_user = regex.match(PATT_GROUPS, url)
+            gruop_id = ''.join(key_user.groups())
+
+
+            user_info['user-info']['info'] = gruop_id
+            user_info['user-info']['type'] = 'gid'
+
+        else:
+            url_parse = parse_qs(urls[1])
+
+            if url_parse:
+                user_info['user-info']['info'] = url_parse.get('id', '')[0]
+                user_info['user-info']['type'] = 'id'
+            else:
+                user_info['user-info']['info'] = urls[1]
+                user_info['user-info']['type'] = 'user'
+
+        return user_info
+
+    def main(self, db_name, ignore_list=None):
+
+        client = MongoClient()
+        db = client.get_database(db_name)
+        collection = db.get_collection('98_album_no_duplicate')
+
+        docs_post = collection.find()
+        time_all_start = time()
+
+        log(f':PHOTO_TANK_IGNORE: Docs in collection = {collection.estimated_document_count()}')
+
+        for doc in docs_post:
+            try:
+                gx = doc.get('photo-cluster')[0].get('photos')[0]
+            except:
+                print(doc.get('photo-cluster')[0].get('url'))
+            user_info = self.get_user_info(gx)
+            user_current = user_info['user-info']['info']
+            if ignore_list and user_current in ignore_list:
+                user_info['user-info']['ignore'] = True
+
+            print(user_info)
+            # collection.update({'_id': doc.get('_id')}, {'$set': user_info})
+
+        print('Finished', time() - time_all_start)
+
+
+if __name__ == '__main__':
+    """
+    TODO: 630423 - แก้ปัญหา https://m.facebook.com/media/set/?set=pcb.745428045950253 with firefox โดนรีพอทว่าสแปมด้วย
+    """
+
+
+    nno = FBTA11001UserInfoAlbum()
+    nno.main('fbta_20200423_0244', [])
